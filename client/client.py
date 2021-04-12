2701a6dd9ada5c0ecf38ea5a1a7afd41f8709855
pranithkallakuri <f20180249@hyderabad.bits-pilani.ac.in>
2021-04-12 17:43:00 +0530

    check-1

--- client/client.py
+++ client/client.py
@@ -1,7 +1,17 @@
 import socket
 
-# create an INET, STREAMing socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# now connect to the web server on port 80 - the normal http port
-s.connect((socket.gethostname(), 10000))
+#def recvFile(sock):
 
+
+# create an INET, STREAMing socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# now connect to the web server on port 80 - the normal http port
+sock.sendto("Hello".encode('utf-8'), ('localhost', 10000))
+
+# recvFile() (receives data packets (client))
+# sendFile() (sends data packets (server))
+
+k = sock.recvfrom(512)
+#print(k[0].decode('utf-8'))
+print(k[0])
+#connect(filename, ip, port)
--- server/server.py
+++ server/server.py
@@ -1,13 +1,44 @@
+import sys
 import socket
 
+window_size = 4
+seq_num = 0
+packet_size = 512
+sr_buffer = [None]*window_size
+
+def sendFile(filename, serversocket, clientaddress):
+    buffer = bytearray()
+    buffer.extend(b'3')
+    buffer.extend(seq_num.to_bytes(4, byteorder='big'))
+    print(buffer)
+    file1 = open(filename, 'r')
+    n = file1.read(507)
+    buffer.extend(n.encode('utf-8'))
+    testsendlen = len(buffer)
+    print("testsendlen = ", testsendlen)
+
+    totalsent = 0
+    while totalsent < 512:
+        sent = serversocket.sendto(buffer[totalsent:], (clientaddress))
+        totalsent += sent
+
+    print("totalsent = ", totalsent)
+    file1.close()
+
+
+#def sendFile()
 #socket.close()
 # create an INET, STREAMing socket
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # bind the socket to a public host, and a well-known port
-serversocket.bind((socket.gethostname(), 10000))
+serversocket.bind(("localhost", 10000))
 print(socket.gethostname())
 # become a server socket
-serversocket.listen(5)
+print("Server started at %s; listening at port %d;" % (serversocket.getsockname()))
+(clientdata, clientaddress) = serversocket.recvfrom(1024)
+print(clientaddress)
+print(clientdata, len(clientdata))
 
-serversocket.accept()
+sendFile("test.txt", serversocket, clientaddress)
+
 serversocket.close()
